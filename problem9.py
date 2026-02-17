8c07dba76eb01bea668bbc57f6b68dc02b66de24
hassan nawaz <hassonawaz2007@gmail.com>
2026-02-17 14:27:13 +0500

    practice problem 9

---
+++ problem9.py
@@ -0,0 +1,5 @@
+a=int(input("enter your number: "))
+
+print("the square of the numbers is",a**2)
+print("the square of the numbers is",a*a)
+# print("the square of the numbers is",a^2) incorrect for finding the square of a number in python
