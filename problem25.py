0bb41efbea9ee2040c35a570f970edef96b6466d
hassan nawaz <hassonawaz2007@gmail.com>
2026-03-11 23:59:44 +0500

    practice problem 26

--- problem25.py
+++ problem25.py
@@ -21,3 +21,6 @@ lang = input("Enter language name:")
 d.update ({name:lang})
 
 print(d)
+
+#the values entered later will be updated
+#Nothing will happpen.The values can be same
---
+++ problem26.py
@@ -0,0 +1,3 @@
+s={8,7,12,"harry",[1,2]}
+
+#no we cannot change the values inside a list which is contained in set S
