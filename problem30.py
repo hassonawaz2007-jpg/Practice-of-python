ebed5ed1b2128aa9f8ed50792418ceefe7f4491c
hassan nawaz <hassonawaz2007@gmail.com>
2026-03-24 15:13:26 +0500

    wrote a porogram in which finding out the name is present in the list or not

--- problem30.py
+++ problem30.py
@@ -4,4 +4,4 @@ if(len(username)<10):
     print("your username contain less than 10 chaacters")
     
 else:
-    print("ALL is WELL!")
+    print("your username contain more than or equal 10 chaacter!")
---
+++ problem31.py
@@ -0,0 +1,9 @@
+l ={"hassan", "musab", "hussain", "mahrukh", "mustafa"}
+
+name =input("enter your name: ")
+
+if(name in l):
+    print("your name is in the list")
+    
+else:
+    print("your name is not in the list")
