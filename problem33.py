dd376800a0267b7aba5578b5399e82c2992cea9b
hassan nawaz <hassonawaz2007@gmail.com>
2026-03-24 15:50:39 +0500

    wrote a porogram to find that it is talking about hassan or not

---
+++ problem33.py
@@ -0,0 +1,7 @@
+post =input("enter the post: ")
+
+if("hassan".upper() in post.upper()):
+    print("this post is talking about harry")
+    
+else:
+    print("this post is not talking about harry")
