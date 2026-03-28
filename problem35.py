0c391522a1fbfd04163ca4a37735cbbc581eefeb
hassan nawaz <hassonawaz2007@gmail.com>
2026-03-28 11:47:01 +0500

    used while loop

--- problem35.py
+++ problem35.py
@@ -3,3 +3,11 @@ l=["hassan", "Sohail","Sidra","musab"]
 for name in l:
     if(name.startswith("S")):
         print(f"hello {name}")
+        
+        
+#l=["hassan", "Sohail","Sidra","musab"]
+#for name in l:
+    #if(name.startswith("S")):
+        #print(f"helllo{name}")
+        
+        
