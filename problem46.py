0b17f6c2d3ec91dc961451bdb497d2fcf2742376
hassan nawaz <hassonawaz2007@gmail.com>
2026-03-31 15:51:06 +0500

    wrote a a porogram by using functions to convert fahrenheit to celsius

---
+++ problem46.py
@@ -0,0 +1,21 @@
+def f_to_c(f):
+    return 5*(f-32)/9  #formula to convert fahrenheit to celsius
+
+f=int(input("enter temperature in f: "))
+c=f_to_c(f)
+print(f"{round(c,2)}°C")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
