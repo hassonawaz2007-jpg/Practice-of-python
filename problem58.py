ebcadec0878b20efd6b1860887b6daba73b0f413
hassan nawaz <hassonawaz2007@gmail.com>
2026-04-18 11:44:50 +0500

    chapter 9 practice problem 5

---
+++ problem58.py
@@ -0,0 +1,9 @@
+word=["donkey","ganda","bad"]
+
+with open("file.txt","r") as f:
+    content=f.read()
+for word in word:
+    content=content.replace(word,"#"*len(word))
+
+with open("file.txt","w") as f:
+    content=f.write(content)
