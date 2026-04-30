3e71c945d74ed9f925d8c1159fdef9f5e1b640a2
hassan nawaz <hassonawaz2007@gmail.com>
2026-04-30 22:57:45 +0500

    chapter 10 practice problem 6

---
+++ problem70.py
@@ -0,0 +1,26 @@
+from random import randint
+
+class Train:
+    
+    def __init__(slf, trainno):
+        slf.trainno = trainno
+    
+    def book(hassan, fro, to):
+        print(f"Ticket is booked in train no: {hassan.trainno} from {fro} to {to}")
+        
+    def getstatus(self):
+        print(f"Train no: {self.trainno} is running on time")
+        
+    def getfare(self, fro, to):
+        print(f"Ticket fare in train no: {self.trainno} from {fro} to {to} is {randint(222, 5555)}")
+        
+
+t = Train(12345)
+t.book("Islamabad", "Karachi")
+t.getstatus()
+t.getfare("islamabad","karachi")
+
+       
+    
+        
+        
