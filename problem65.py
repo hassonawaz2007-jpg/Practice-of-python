557380ee7ead8d17cdbfa773210ea246e44cbd39
hassan nawaz <hassonawaz2007@gmail.com>
2026-04-30 21:27:08 +0500

    chapter 10 practice problem 1

---
+++ problem65.py
@@ -0,0 +1,12 @@
+class porogrammer:
+    company="Microsoft"
+    def __init__(self,name,salary,pin):
+        self.name=name 
+        self.salary=salary
+        self.pin=pin
+        
+p=porogrammer("hassan",1200000,23455)
+print(p.name,p.salary,p.pin)
+
+m=porogrammer("musab",1200000,27555)
+print(m.name,m.salary,m.pin)
