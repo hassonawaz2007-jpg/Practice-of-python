23f6bc266d795bd6cf370680572930f6849bfd9a
hassan nawaz <hassonawaz2007@gmail.com>
2026-06-11 19:27:39 +0500

    practice problem 82

---
+++ problem82.py
@@ -0,0 +1,7 @@
+name= input("enter name: ")
+marks= int(input("enter marks: "))
+phone= int(input("phone number: "))
+
+s="the name of the student is {},his marks are {} and phone number is {}".format(name,marks,phone)
+
+print(s)
